7efff731732b5664ef3d8ece41beb53364d9d599
andynack <andrew.nack@gmail.com>
2014-11-16 22:47:43 -0500

    Starting Point, Functionality of app is basic video streaming with preview shown on phone screen.

---
+++ gen/edu/utk/friendlyflashlight/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.utk.friendlyflashlight;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/utk/friendlyflashlight/AppConfig.java
@@ -0,0 +1,7 @@
+package edu.utk.friendlyflashlight;
+
+public class AppConfig {
+	public static final String STREAM_URL = "rtsp://10.0.0.30:1935/live/ff_leak";
+    public static final String PUBLISHER_USERNAME = "andyandroid";
+    public static final String PUBLISHER_PASSWORD = "abunchof1sand0s";
+}
