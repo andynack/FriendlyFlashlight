507cb85b023265cab5777d4f9c22264fc3183dd8
andynack <andrew.nack@gmail.com>
2014-11-20 23:51:14 -0500

    video stream with flashlight functional

--- src/edu/utk/friendlyflashlight/MainActivity.java
+++ src/edu/utk/friendlyflashlight/MainActivity.java
@@ -8,31 +8,22 @@ import net.majorkernelpanic.streaming.SessionBuilder;
 import net.majorkernelpanic.streaming.audio.AudioQuality;
 import net.majorkernelpanic.streaming.gl.SurfaceView;
 import net.majorkernelpanic.streaming.rtsp.RtspClient;
-import net.majorkernelpanic.streaming.video.VideoQuality;
+//import net.majorkernelpanic.streaming.video.VideoQuality;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
-@SuppressWarnings({ "unused", "deprecation" })
-public class MainActivity extends Activity implements RtspClient.Callback, Session.Callback, SurfaceHolder.Callback {
+public class MainActivity extends Activity implements OnClickListener, RtspClient.Callback, Session.Callback, SurfaceHolder.Callback {
 
 	ImageButton btnSwitch;
-	
-//	private Camera camera;
-    private boolean isFlashOn;
-    private boolean hasFlash;
-//  Parameters params;
-    
+	    
 	// log tag
 	public final static String TAG = MainActivity.class.getSimpleName();
 	
@@ -47,9 +38,7 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
 		setContentView(R.layout.activity_main);
 		
 		mSurfaceView = (SurfaceView) findViewById(R.id.surface);
@@ -61,103 +50,30 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
 		
 		// flash switch button
         btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
-		
-        // get the camera
-//      getCamera();
-         
-        // displaying button image
-//        toggleButtonImage();
-                  
+        btnSwitch.setTag("off");
+ 
         // Switch button click event to toggle flash on/off
-    /*    btnSwitch.setOnClickListener(new View.OnClickListener() {
+        btnSwitch.setOnClickListener(this);
 
-            @Override
-            public void onClick(View v) {
-                if (isFlashOn) {
-                    // turn off flash
-                    turnOffFlash();
-                } else {
-                    // turn on flash
-                    turnOnFlash();
-                }
-            }
-        });*/
 	}
 	
-	// getting camera parameters
-/*	private void getCamera() {
-	    if (camera == null) {
-	        try {
-	            camera = Camera.open();
-	            params = camera.getParameters();
-	        } catch (RuntimeException e) {
-	            Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
-	        }
-	    }
-	}*/
-	
-	 /*
-	 * Turning On flash
-	 */
-/*	private void turnOnFlash() {
-	    if (!isFlashOn) {
-	        if (camera == null || params == null) {
-	            return;
-	        }
-	        
-	        params = camera.getParameters();
-	        params.setFlashMode(Parameters.FLASH_MODE_TORCH);
-	        camera.setParameters(params);
-	        camera.startPreview();
-	        isFlashOn = true;
-	         
-	        // changing button/switch image
-	        toggleButtonImage();
-	    }
-	 
-	}*/
-	
-	/*
-	 * Turning Off flash
-	 */
-/*	private void turnOffFlash() {
-	    if (isFlashOn) {
-	        if (camera == null || params == null) {
-	            return;
-	        }
-	        
-	        params = camera.getParameters();
-	        params.setFlashMode(Parameters.FLASH_MODE_OFF);
-	        camera.setParameters(params);
-	        camera.stopPreview();
-	        isFlashOn = false;
-	         
-	        // changing button/switch image
-	        toggleButtonImage();
-	    }
-	}*/
-		
-	/*
-	 * Toggle switch button images
-	 * changing image states to on / off
-	 * */
-/*	private void toggleButtonImage(){
-	    if(isFlashOn){
-	        btnSwitch.setImageResource(R.drawable.btn_switch_on);
-	    }else{
-	        btnSwitch.setImageResource(R.drawable.btn_switch_off);
-	    }
-	}*/
-    
+	public void onClick(View v) {
+		if (btnSwitch.getTag().equals("on")) {
+			btnSwitch.setTag("off");
+			btnSwitch.setImageResource(R.drawable.btn_switch_off);
+		} else {
+			btnSwitch.setImageResource(R.drawable.btn_switch_on);
+			btnSwitch.setTag("on");
+		}
+		mSession.toggleFlash();
+	}
+
 	@Override
 	protected void onResume() {
         super.onResume();
          
         toggleStreaming();
         
-        // on resume turn on the flash
-	/*    if(hasFlash)
-	        turnOnFlash();*/
     }
 	
 	@Override
@@ -166,8 +82,6 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
          
         toggleStreaming();
         
-        // on pause turn off the flash
-//	    turnOffFlash();
     }
 	
 	private void initRtspClient() {
@@ -224,12 +138,6 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
         mSession.release();
         mSurfaceView.getHolder().removeCallback(this);
     }
-	
- /*   @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    } */
     
     @Override
     public void onSessionError(int reason, int streamType, Exception e) {
@@ -237,6 +145,8 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
         case Session.ERROR_CAMERA_ALREADY_IN_USE:
             break;
         case Session.ERROR_CAMERA_HAS_NO_FLASH:
+        	btnSwitch.setTag("off");
+			btnSwitch.setImageResource(R.drawable.btn_switch_off);
             break;
         case Session.ERROR_INVALID_SURFACE:
             break;
@@ -303,12 +213,12 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
  
     @Override
     public void surfaceCreated(SurfaceHolder arg0) {
- 
+    	mSession.startPreview(); 
     }
  
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
- 
+    	mClient.stopStream();
     }
  
     @Override
@@ -316,22 +226,4 @@ public class MainActivity extends Activity implements RtspClient.Callback, Sessi
  
     }
     
- /*   protected void onStart() {
-	    super.onStart();
-	     
-	    // on starting the app get the camera params
-	    getCamera();
-	}
-	 
-	@Override
-	protected void onStop() {
-	    super.onStop();
-	     
-	    // on stop release the camera
-	    if (camera != null) {
-	        camera.release();
-	        camera = null;
-	    }
-	}*/
- 
 }
